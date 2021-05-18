907d184e951fc8e24cc228c3bae2c07e09b36ddc
rrrui0812 <1051247109@nkust.edu.tw>
2021-05-18 14:52:07 +0800

    edited comments_table column name

---
+++ database/migrations/2021_05_18_143505_edit_column_name_comments.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditColumnNameComments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->renameColumn('comment_id','parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
+    }
+}
